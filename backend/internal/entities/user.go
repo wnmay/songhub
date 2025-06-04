f9a5b49e69792566ae6ca345cadb18fe058a4c6c
wnmay <wanathachat@gmail.com>
2025-06-04 23:07:16 +0700

    feat: add json struct

--- backend/internal/entities/user.go
+++ backend/internal/entities/user.go
@@ -6,8 +6,8 @@ import (
 
 type User struct {
 	gorm.Model
-	Username string   `gorm:"unique;not null"`
-	Email    string   `gorm:"unique;not null"`
-	Password string   `gorm:"not null"`
-	Role     UserRole `gorm:"type:varchar(20);not null;default:'customer'"`
+	Username string   `gorm:"unique;not null" json:"username"`
+	Email    string   `gorm:"unique;not null" json:"email"`
+	Password string   `gorm:"not null" json:"password"`
+	Role     UserRole `gorm:"type:varchar(20);not null;default:'customer'" json:"role"`
 }
